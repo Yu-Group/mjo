3dc179a279c97f610667142234a4e1b98f64e7a4
James Duncan <jpduncan@berkeley.edu>
2022-03-30 23:41:50 -0700

    Add ERA5 viz util

--- mjonet/viz.py
+++ mjonet/viz.py
@@ -4,12 +4,13 @@ from cartopy import crs as ccrs
 
 import numpy as np
 
-def plot_era5(*darrays, extent=None, figsize=(10, 20)):
+def plot_era5(*darrays, extent=None):
     if extent is None:
         extent = [-75, 85, -15, 75]
 
     lat = darrays[0]['lat'][:]
     lon = darrays[0]['lon'][:]
+    ratio = lon.size / lat.size
 
     # extract longitude extent (recall that lon ranges from 0 to 360 in the ERA5 data)
     x_idx = np.intersect1d(np.where(lon >= extent[0] + 180), np.where(lon <= extent[1] + 180))
@@ -19,15 +20,22 @@ def plot_era5(*darrays, extent=None, figsize=(10, 20)):
     y_idx = np.intersect1d(np.where(lat >= extent[2]), np.where(lat <= extent[3]))
     y = lat[y_idx]
 
-    fig, axs = plt.subplots(ncols=4, squeeze=False, figsize=figsize)
+    R, C = len(darrays) // 2 + len(darrays) % 2, min(len(darrays), 2)
+    figsize = (8*C, 5*R)
+    proj = ccrs.PlateCarree(central_longitude=-180.0)
+    fig, axs = plt.subplots(nrows=R, ncols=C, 
+                            sharex=True, sharey=True, 
+                            subplot_kw={'projection': proj}, 
+                            figsize=figsize)#,squeeze=False)
 
     for i, darray in enumerate(darrays):
-        proj = ccrs.PlateCarree(central_longitude=-180.0)
-        ax = fig.add_subplot(i // 4, i % 4, i, projection=proj)
+        # import pdb; pdb.set_trace()
+        ax = plt.subplot(R, C, i + 1)
         ax.set_extent(extent, crs=proj)
         ax.coastlines()
         ax.gridlines(draw_labels=True, crs=proj)
-        filled_c = ax.contourf(x, y, darrays[i][y_idx, :][:, x_idx], transform=ccrs.PlateCarree())
-        fig.colorbar(filled_c, orientation='vertical')
+        ax.title.set_text(darray.short_name)
+        filled_c = ax.contourf(x, y, darray[y_idx, :][:, x_idx], transform=ccrs.PlateCarree())
+        plt.colorbar(filled_c, orientation='vertical', ax=ax, fraction=ratio*0.046, pad=0.04)
 
     plt.show()
